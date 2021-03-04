608fd92982d8a99399a655501984b76aded09594
Davide Napoletano <davide.napoletano@cern.ch>
2021-03-04 16:51:09 +0100

    progress

--- src/Configuration.cpp
+++ src/Configuration.cpp
@@ -47,7 +47,7 @@ void Configuration::CalcEnergy()
       const double d{Atom::Distance((*it1), (*it2), LCube)};
       if(d < Constants::rCutoff and d != 0){
         PotEnergy += WanderVaals(d);
-        virial  += d * WanderVaalsForce(d);
+        virial  += d * abs(WanderVaalsForce(d));
       }
     }
   }
